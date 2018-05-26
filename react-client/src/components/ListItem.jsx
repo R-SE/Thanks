9c7925accf5cbe1794fa8ae783272e76bc4d5ff8
Hack Reactor Student <student@hackreactor.com>
2018-05-26 00:59:33 -0400

    stop automatic rerender on bubbles causing jump bug

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ListItem = (props) => {
+/*const ListItem = (props) => {
   let bubbleStyle = {
     top: `${(Math.random() * (document.documentElement.clientHeight -100))+50}px`,
     left: `${(Math.random() * (document.documentElement.clientWidth - 100))+50}px`,
@@ -12,9 +12,30 @@ const ListItem = (props) => {
     { props.shoutout.text }
   </div>
 )
+}*/
+
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState){
+  //  return !equals(nextProps, this.props);
+  return false;
+  }
+  render() {
+      let bubbleStyle = {
+    top: `${(Math.random() * (document.documentElement.clientHeight -100))+50}px`,
+    left: `${(Math.random() * (document.documentElement.clientWidth - 100))+50}px`,
+    color: 'blue',
+    // border: '5px solid pink'
+  };
+  return (
+  <div className="bubble" style={bubbleStyle} onMouseOver={e => this.props.updateMessage(e.target.innerHTML)}>
+    { this.props.shoutout.text }
+  </div>
+)
+  }
 }
 
 
 export default ListItem;
-
-// Math.random() * (document.documentElement.clientHeight - 200)
